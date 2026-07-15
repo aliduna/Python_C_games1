dc8e11056860fb2df6773a25125a8fca5f655054
aliduna <oyunmrt@gmail.com>
2026-07-15 17:51:32 +0300

    Change The Codes Project
    İts a code that changes the print fonk. into the name you gave to it.
    
    Bu kod print fonksiyonunu senin girdiğin isme dönüştürmeni sağlar ve o ismi yazarsan print yerine çalışabilir.

---
+++ prints_other_types.py
@@ -0,0 +1,19 @@
+
+# ! Bu kodlardaki fonksiyon isimleri değiştirilebilir. Değiştirilince benim verdiğim örnekler yerine o değiştirilmiş-
+# ! ismi print yerine kullanılması gerekebilir.
+
+#! You can change the fonk. name in the code. İf you change it, you need to use the changed name, not my example
+
+"""
+
+(Tr)  TÜRKÇE KOD
+
+def yazdir(girdi):
+    print(girdi)
+
+yazdir("Merhaba Dünya! ")
+
+(Gb) ENGLİSH CODE
+def say(input):
+    print(input)
+"""
